acd0305e6da0c6f32fb4b55eadf6e6cbd1fc7c9f
Ajasa Adedamola <azeemajasa@gmail.com>
2022-06-05 21:42:04 +0100

    update commit

---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,90 @@
+import json
+import dateutil.parser
+import babel
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from flask_wtf import Form
+from sqlalchemy import ForeignKey
+from forms import *
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:damoxify@localhost:5432/musical'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+
+
+class venue(db.Model):
+    __tablename__ = 'venue'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String()))
+    address = db.Column(db.String(120))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('show', backref='venue', lazy=True)
+
+
+def __repr__(self):
+    return '<venue {}>'.format(self.name)
+
+
+@app.route('/')
+def venues():
+    return render_template('venues.html', data=venue.query.all())
+
+
+class artist(db.Model):
+    __tablename__ = 'artist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('show', backref='artist', lazy=True)
+
+    def __repr__(self):
+        return '<artist {}>'.format(self.name)
+
+
+@app.route('/')
+def artists():
+    return render_template('artists.html', data=artist.query.all())
+
+
+class show(db.Model):
+    __tablename__ = 'show'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+
+
+def __repr__(self):
+    return '<show {}{}>'.format(self.artist_id, self.venue_id)
+
+
+@app.route('/')
+def shows():
+    return render_template('shows.html', data=show.query.all())
